c73971d928cb59e1b82782486db30fde25f613c0
Moises Torres <moises.puc@adh.com>
2023-10-09 15:32:05 -0500

    Agregar archivos de proyecto.

---
+++ 24CV-WEB/Controllers/EjemplosController.cs
@@ -0,0 +1,23 @@
+﻿using _24CV_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _24CV_WEB.Controllers
+{
+    public class EjemplosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Moisés";
+            persona.Apellidos = "Torres";
+            persona.FechaNacimiento = new DateTime(1992, 01, 15);
+
+            return View(persona);
+        }
+    }
+}
---
+++ 24CV-WEB/Models/Persona.cs
@@ -0,0 +1,9 @@
+﻿namespace _24CV_WEB.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public  DateTime FechaNacimiento { get; set; }
+    }
+}
